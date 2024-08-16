46de1fa63a8ddfa36cfe759814a3f45e1db70814
s3042HJX <2038392181@qq.com>
2024-08-16 18:16:20 +0800

    Update leaderboard.tsx

--- src/view/leaderboard.tsx
+++ src/view/leaderboard.tsx
@@ -44,7 +44,7 @@ export default function Leaderboard() {
                     className="!text-[0.86rem]"
                     style={{ textShadow: " 2px 2px 4px rgba(0, 0, 0, 0.5)" }}
                   >
-                    {userRank.rank | 1}
+                    {userRank.rank-1 | 0}
                   </Text>
                 </div>
               ) : (
