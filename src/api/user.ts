86fa18c281beec8aee6a3af852f84e7c83eedd8e
jay <3334354196@qq.com>
2024-08-30 17:54:51 +0800

    merge code

--- src/api/user.ts
+++ src/api/user.ts
@@ -42,8 +42,5 @@ export const bindWallentAPI = (
   data?: string,
   token?: string
 ) => {
-
-  // return `https://skeleton-8wov.vercel.app/${type}/${data}/${token}`
-
-  return `https://test.pi.sale/${type}/${data}/${token}`
+  return `https://skeleton-8wov.vercel.app/${type}/${data}/${token}`
 }
