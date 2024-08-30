598ea126c5ec7ee943a25e456b7bd95dbd6ba70a
Slerf CTO $SLERF <pi@space-pi.com>
2024-08-30 18:32:04 +0800

    Update index.tsx

--- src/provider/telegram/index.tsx
+++ src/provider/telegram/index.tsx
@@ -21,7 +21,27 @@ export const TelegramContext = createContext<{
   }
 }>({})
 
-const initInfo = {}
+const initInfo = import.meta.env.DEV
+  ? {
+    initData:
+    "user=%7B%22id%22%3A1875953573%2C%22first_name%22%3A%22NoBody%22%2C%22last_name%22%3A%22%22%2C%22username%22%3A%22NoBodyDao2099%22%2C%22language_code%22%3A%22zh-hans%22%2C%22is_premium%22%3Atrue%2C%22allows_write_to_pm%22%3Atrue%7D&chat_instance=-3226193064720846214&chat_type=supergroup&auth_date=1725013635&hash=61b3d11af2a613a15890a5dbc32c377d8956e483745fe6df4e81572fa96ef324",
+  initDataUnsafe: {
+    user: {
+      id: 1875953573,
+      first_name: "NoBody",
+      last_name: "",
+      username: "NoBodyDao2099",
+      language_code: "zh-hans",
+      is_premium: true,
+      allows_write_to_pm: true,
+    },
+    chat_instance: "-3226193064720846214",
+    chat_type: "supergroup",
+    auth_date: "1725013635",
+    hash: "61b3d11af2a613a15890a5dbc32c377d8956e483745fe6df4e81572fa96ef324",
+  },
+    }
+  : {}
 
 export const Telegram = ({ children }: { children?: React.ReactNode }) => {
   // const useViewport = useViewportRaw(true)?.result;
