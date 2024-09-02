c5302102628a49c2a5669d2ee4eed1f761a35a58
jay <3334354196@qq.com>
2024-09-02 18:05:14 +0800

    upload

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -86,9 +86,7 @@ const Steps = ({
   const lock = useRef<boolean>(false)
   const { telegramUserData, friendRank } = useAppSelector(state => state.user)
   const { webApp } = useTelegram() as any
-  useEffect(() => {
-    webApp && webApp.disableVerticalSwipes && webApp.disableVerticalSwipes()
-  }, [webApp])
+
   const login = async () => {
     if (lock.current || !postData) return
     lock.current = true
