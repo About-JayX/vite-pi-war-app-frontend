64c95b2d5146454aea4217ba9e97994070768d56
jay <3334354196@qq.com>
2024-09-03 18:59:06 +0800

    upload load data

--- src/view/friends/index.tsx
+++ src/view/friends/index.tsx
@@ -1,21 +1,22 @@
 import Button from '@/components/button'
 import { HeaderTitle, Title } from '@/components/title'
-import { useAppSelector } from '@/store/hook'
+import { useAppDispatch, useAppSelector } from '@/store/hook'
 import { CardHeader, Container } from '@material-ui/core'
 import { useTranslation } from 'react-i18next'
 import { getTextColorForBackground, semicolon, stringToColor } from '@/utils'
 import { Text } from '@/components/text'
 import Loader from '@/components/loader'
 import Share from './share'
-import { useState } from 'preact/hooks'
+import { useEffect, useState } from 'preact/hooks'
 import { Avatar } from '../leaderboard'
 import Box from '@/components/box'
+import { List } from 'react-vant'
+import api from '@/api'
+import { updateFriendRank } from '@/store/user'
 
 export default function Friends() {
   const { t } = useTranslation()
-
   const { telegramUserData, friendRank } = useAppSelector(state => state.user)
-
   const [open, setOpen] = useState<boolean>(false)
 
   const shareUrl = `https://t.me/share/url?url=https://t.me/${
@@ -23,10 +24,35 @@ export default function Friends() {
   }/?startapp%3D${
     telegramUserData.Invitation_code
   }&text=%0A${encodeURIComponent(t('friends.inviteText'))}`
-
   const nav: string[] = t('nav', { returnObjects: true })
-
-  const [loading, setLoading] = useState(false)
+  const dispatch = useAppDispatch()
+  const [finished, setFinished] = useState(true)
+  const loadData = async () => {
+    const page = friendRank.page + 1
+    const pageSize = friendRank.pageSize
+    const result = await api.user.friendRankAPI({ page, pageSize })
+    const nArr = [...friendRank.friends, ...result.data.friends]
+    dispatch(
+      updateFriendRank({
+        page,
+        pageSize,
+        friends: nArr,
+        total: result.data.total,
+      })
+    )
+  }
+  useEffect(() => {
+    if (
+      friendRank &&
+      friendRank.friends &&
+      friendRank.friends.length &&
+      friendRank.friends.length < friendRank.total
+    ) {
+      setFinished(false)
+    } else {
+      setFinished(true)
+    }
+  }, [friendRank, finished])
   return (
     <>
       <Share open={open} onHide={() => setOpen(false)} url={shareUrl} />
@@ -45,36 +71,43 @@ export default function Friends() {
               <HeaderTitle className="text-left w-full">
                 {friendRank.total || 0} {t('public.friends')}
               </HeaderTitle>
-              {friendRank.friends &&
-                friendRank.friends.length > 0 &&
-                friendRank.friends.map((item: any, index: number) => (
-                  <CardHeader
-                    key={index}
-                    className="w-full !p-0"
-                    avatar={
-                      <Avatar
-                        name={
-                          (item.invited_by_userName &&
-                            item.invited_by_userName
-                              .slice(0, 2)
-                              .toUpperCase()) ||
-                          ''
-                        }
-                        bg={stringToColor(item.invited_by_userName)}
-                        color={
-                          getTextColorForBackground(item.invited_by_userName)
-                            .textColor
-                        }
-                      />
-                    }
-                    action={
-                      <Text>
-                        +{semicolon(item.reward_amount || 0)}&nbsp;PIS
-                      </Text>
-                    }
-                    title={<Text>{item.invited_by_userName || ''}</Text>}
-                  />
-                ))}
+              <List
+                onLoad={loadData}
+                finished={finished}
+                className="grid gap-6 w-100 justify-items-center"
+              >
+                {' '}
+                {friendRank.friends &&
+                  friendRank.friends.length > 0 &&
+                  friendRank.friends.map((item: any, index: number) => (
+                    <CardHeader
+                      key={index}
+                      className="w-full !p-0"
+                      avatar={
+                        <Avatar
+                          name={
+                            (item.invited_by_userName &&
+                              item.invited_by_userName
+                                .slice(0, 2)
+                                .toUpperCase()) ||
+                            ''
+                          }
+                          bg={stringToColor(item.invited_by_userName)}
+                          color={
+                            getTextColorForBackground(item.invited_by_userName)
+                              .textColor
+                          }
+                        />
+                      }
+                      action={
+                        <Text>
+                          +{semicolon(item.reward_amount || 0)}&nbsp;PIS
+                        </Text>
+                      }
+                      title={<Text>{item.invited_by_userName || ''}</Text>}
+                    />
+                  ))}
+              </List>
             </div>
             <Loader />
           </div>
--- src/view/leaderboard.tsx
+++ src/view/leaderboard.tsx
@@ -1,13 +1,16 @@
+import api from '@/api'
 import Box from '@/components/box'
 import Icon from '@/components/icon'
 import Loader from '@/components/loader'
 import { Text } from '@/components/text'
 import { HeaderTitle, Title } from '@/components/title'
-import { useAppSelector } from '@/store/hook'
+import { useAppDispatch, useAppSelector } from '@/store/hook'
+import { updateInviteRank } from '@/store/user'
 import { getTextColorForBackground, semicolon, stringToColor } from '@/utils'
 import { Card, CardHeader, Container } from '@material-ui/core'
-import { useState } from 'preact/hooks'
+import { useEffect, useState } from 'preact/hooks'
 import { useTranslation } from 'react-i18next'
+import { List } from 'react-vant'
 
 export const Avatar = ({
   name = '',
@@ -63,19 +66,35 @@ const getNameIcon = (index: number) => {
       return index
   }
 }
-const onRefresh = (showToast: any) => {
-  return new Promise(resolve => {
-    setTimeout(() => {
-      if (showToast) {
-      }
-      resolve(true)
-    }, 1000)
-  })
-}
+
 export default function Leaderboard() {
   const { t } = useTranslation()
   const { userRank, inviteRank } = useAppSelector(state => state.user)
+  const dispatch = useAppDispatch()
+  const [finished, setFinished] = useState(true)
+  const loadData = async () => {
+    const page = inviteRank.page + 1
+    const pageSize = inviteRank.pageSize
 
+    const result = await api.user.inviteRankAPI({ page, pageSize })
+    const nArr = [...inviteRank.data, ...result.data.data]
+    dispatch(
+      updateInviteRank({ page, pageSize, data: nArr, total: result.data.total })
+    )
+  }
+  useEffect(() => {
+    if (
+      inviteRank &&
+      inviteRank.data &&
+      inviteRank.data.length &&
+      inviteRank.data.length < 500 &&
+      inviteRank.data.length < inviteRank.total
+    ) {
+      setFinished(false)
+    } else {
+      setFinished(true)
+    }
+  }, [inviteRank])
   return (
     <Box>
       <div className="grid gap-6 w-100 justify-items-center">
@@ -127,53 +146,61 @@ export default function Leaderboard() {
         <HeaderTitle className="w-full">
           {inviteRank.total || 0} {t('public.holders')}
         </HeaderTitle>
-        {inviteRank.data &&
-          inviteRank.data.length &&
-          inviteRank.data.map((item: any, index: number) => (
-            <CardHeader
-              key={index}
-              className="w-full !p-0"
-              avatar={
-                <Avatar
-                  name={
-                    (item.username &&
-                      item.username.slice(0, 2).toUpperCase()) ||
-                    ''
-                  }
-                  bg={stringToColor(item.username || '')}
-                  color={
-                    getTextColorForBackground(item.username).textColor || ''
-                  }
-                />
-              }
-              action={
-                index <= 10 ? (
-                  <div className="w-[30px] h-[45px] relative flex items-center justify-center">
-                    {(item.rank || index + 1) <= 3 ? (
-                      getNameIcon(item.rank || index + 1)
-                    ) : (
-                      <Text
-                        className="!text-[0.86rem]"
-                        style={{
-                          textShadow: ' 2px 2px 4px rgba(0, 0, 0, 0.5)',
-                        }}
-                      >
-                        {'#' + (item.rank || index + 1)}
-                      </Text>
-                    )}
-                  </div>
-                ) : (
-                  item.rank || index + 1
-                )
-              }
-              title={<Text>{item.username || ''}</Text>}
-              subheader={
-                <Text className="text-white/50">
-                  {semicolon(item.score || 0)} PIS
-                </Text>
-              }
-            />
-          ))}
+        <List
+          offset={100}
+          onLoad={loadData}
+          finished={finished}
+          className="grid gap-6 w-100 justify-items-center"
+        >
+          {inviteRank.data &&
+            inviteRank.data.length &&
+            inviteRank.data.map((item: any, index: number) => (
+              <CardHeader
+                key={index}
+                className="w-full !p-0"
+                avatar={
+                  <Avatar
+                    name={
+                      (item.username &&
+                        item.username.slice(0, 2).toUpperCase()) ||
+                      ''
+                    }
+                    bg={stringToColor(item.username || '')}
+                    color={
+                      getTextColorForBackground(item.username).textColor || ''
+                    }
+                  />
+                }
+                action={
+                  index <= 10 ? (
+                    <div className="w-[30px] h-[45px] relative flex items-center justify-center">
+                      {(item.rank || index + 1) <= 3 ? (
+                        getNameIcon(item.rank || index + 1)
+                      ) : (
+                        <Text
+                          className="!text-[0.86rem]"
+                          style={{
+                            textShadow: ' 2px 2px 4px rgba(0, 0, 0, 0.5)',
+                          }}
+                        >
+                          {'#' + (item.rank || index + 1)}
+                        </Text>
+                      )}
+                    </div>
+                  ) : (
+                    item.rank || index + 1
+                  )
+                }
+                title={<Text>{item.username || ''}</Text>}
+                subheader={
+                  <Text className="text-white/50">
+                    {semicolon(item.score || 0)} PIS
+                  </Text>
+                }
+              />
+            ))}
+        </List>
+
         <Loader />
       </div>
     </Box>
