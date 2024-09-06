aa8e3741ac6d3be28831e67e0d939192fe8cb15f
jay <3334354196@qq.com>
2024-09-06 11:17:11 +0800

    upload code

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -93,14 +93,13 @@ const Steps = ({
         (result.data && result.data.authToken) || ''
       )
 
-      // !result.data.isNewUser &&
-      //   (await initData(dispatch, postData, () => {
-      //     setInitLock(false)
-      //   }))
+      !result.data.isNewUser &&
+        (await initData(dispatch, postData, () => {
+          setInitLock(false)
+        }))
 
-      // result.data.isNewUser && 
-      onChange && onChange(1)
-      // dispatch(updateNewUser(result.data && result.data.isNewUser))
+      result.data.isNewUser && onChange && onChange(1)
+      dispatch(updateNewUser(result.data && result.data.isNewUser))
     } catch (error) {
       console.log(error, 'error_')
     }
@@ -409,7 +408,9 @@ const Steps = ({
               {semicolon(telegramUserData.gold) || 0} PIS
             </Text>
           </div>
-          <div className="whitespace-pre-line mb-6">{t('steps.steps4.text2')}</div>
+          <div className="whitespace-pre-line mb-6">
+            {t('steps.steps4.text2')}
+          </div>
           <div class="!bg-black m-[-1rem]  w-full sticky bottom-0 z-1 !mb-2">
             <Button
               className="w-100"
