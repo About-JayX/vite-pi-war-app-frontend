f7c8bc65482741da98d486fae25db789d259062c
jay <3334354196@qq.com>
2024-09-12 19:05:36 +0800

    upload show pid

--- src/view/airdrops/index.tsx
+++ src/view/airdrops/index.tsx
@@ -1,49 +1,49 @@
-import Input from "@/components/input";
-import { Text } from "@/components/text";
-import { HeaderTitle, Title } from "@/components/title";
-import { useAppSelector } from "@/store/hook";
-import { useEffect, useState } from "preact/hooks";
-import { useTranslation } from "react-i18next";
-import api from "@/api";
-import "./index.css";
-import Wallet from "./wallet";
-import Segmented from "@/components/segmented";
-import Icon from "@/components/icon";
-import SuccessPng from "@/assets/icon/success.png";
-import SuccessaPng from "@/assets/icon/success-a.png";
-import { semicolon } from "@/utils";
-import Button from "@/components/button";
-import Loader from "@/components/loader";
-import Box from "@/components/box";
-import PiModal from "./piModal";
+import Input from '@/components/input'
+import { Text } from '@/components/text'
+import { HeaderTitle, Title } from '@/components/title'
+import { useAppSelector } from '@/store/hook'
+import { useEffect, useState } from 'preact/hooks'
+import { useTranslation } from 'react-i18next'
+import api from '@/api'
+import './index.css'
+import Wallet from './wallet'
+import Segmented from '@/components/segmented'
+import Icon from '@/components/icon'
+import SuccessPng from '@/assets/icon/success.png'
+import SuccessaPng from '@/assets/icon/success-a.png'
+import { semicolon } from '@/utils'
+import Button from '@/components/button'
+import Loader from '@/components/loader'
+import Box from '@/components/box'
+import PiModal from './piModal'
 
 export default function Airdrops() {
-  const { t } = useTranslation();
+  const { t } = useTranslation()
   // const { uid } = props?.searchParams
-  const [bindingMethod, setBindingMethod] = useState<string>("Solana");
+  const [bindingMethod, setBindingMethod] = useState<string>('Solana')
 
-  const { bindStatus, userReward } = useAppSelector((state) => state.user);
-  const [walletOpen, setWalletOpen] = useState(false);
-  const [bdLog, setBdLog] = useState([]);
-  const [bindpiModal, setBindpiModal] = useState(false);
+  const { bindStatus, userReward } = useAppSelector(state => state.user)
+  const [walletOpen, setWalletOpen] = useState(false)
+  const [bdLog, setBdLog] = useState([])
+  const [bindpiModal, setBindpiModal] = useState(false)
 
   useEffect(() => {
     if (Object.keys(userReward).length) {
-      console.log(userReward.activityLogs);
+      console.log(userReward.activityLogs)
 
       setBdLog(
         userReward.activityLogs.filter((item: any) =>
-          item.key.includes("Binding")
+          item.key.includes('Binding')
         )
-      );
+      )
     }
-  }, [userReward]);
+  }, [userReward])
   const getUrl = () => {
-    return api.user.bindWallentAPI(bindStatus.Code || "");
-  };
+    return api.user.bindWallentAPI(bindStatus.Code || '')
+  }
   const getAddress = () => {
-    return bindingMethod === "Solana" ? bindStatus.Sonala : bindStatus.Erc20;
-  };
+    return bindingMethod === 'Solana' ? bindStatus.Sonala : bindStatus.Erc20
+  }
 
   const ellipsisMiddle = (
     text: string,
@@ -53,13 +53,13 @@ export default function Airdrops() {
     if (text?.length > maxLength + 6) {
       return (
         text?.slice(0, maxLength) +
-        "..." +
+        '...' +
         text?.slice(text?.length - (mimLength || maxLength))
-      );
+      )
     } else {
-      return text;
+      return text
     }
-  };
+  }
 
   return (
     <>
@@ -77,20 +77,20 @@ export default function Airdrops() {
       />
       <Box>
         <div className="grid gap-6 w-100 justify-items-center text-center">
-          <Title>{t("public.airdrops")}</Title>
-          <Text className="text-color mt-[-1rem]">{t("binding.text")}</Text>
+          <Title>{t('public.airdrops')}</Title>
+          <Text className="text-color mt-[-1rem]">{t('binding.text')}</Text>
           <Text
             className="mt-[-1rem] mb-[-1rem] text-[1rem]"
-            style={{ color: "#0E8EF4" }}
+            style={{ color: '#0E8EF4' }}
           >
-            {t("binding.bindingTipsText")}
+            {t('binding.bindingTipsText')}
           </Text>
           <Input
             placeholder={`${getUrl()}`}
             disabled
             background="#030915"
             button={{
-              text: t("public.copy"),
+              text: t('public.copy'),
               copy: true,
               show: true,
               copyText: `${getUrl()}`,
@@ -99,18 +99,18 @@ export default function Airdrops() {
           <div className="card !p-4 !pt-6 !pb-6 grid gap-3 w-100 binding-card-bg bg-transparent">
             <Segmented
               value={bindingMethod}
-              onChange={(e) => setBindingMethod(e)}
+              onChange={e => setBindingMethod(e)}
               data={[
-                { label: "SOL", value: "Solana" },
-                { label: "ETH/BSC", value: "ETH/BSC" },
-                { label: "Pi", value: "PiBrowser" },
+                { label: 'SOL', value: 'Solana' },
+                { label: 'ETH/BSC', value: 'ETH/BSC' },
+                { label: 'Pi', value: 'PiBrowser' },
               ]}
             />
-            {bindingMethod === "Solana" || bindingMethod === "ETH/BSC" ? (
+            {bindingMethod === 'Solana' || bindingMethod === 'ETH/BSC' ? (
               getAddress() ? (
                 <div className="bind-wallet">
                   <div className="bind-wallet-container">
-                    {bindingMethod === "Solana" ? (
+                    {bindingMethod === 'Solana' ? (
                       <Icon name="sol" />
                     ) : (
                       <Icon name="wallet" />
@@ -127,7 +127,7 @@ export default function Airdrops() {
                 </div>
               ) : (
                 <Button onClick={() => setWalletOpen(true)}>
-                  {t("public.bindWallet")}
+                  {t('public.bindWallet')}
                 </Button>
               )
             ) : (
@@ -135,21 +135,21 @@ export default function Airdrops() {
                 {bindStatus.Pid ? (
                   <div className="bind-wallet">
                     <div className="bind-wallet-container">
-                      <Icon name="piNetwork"/>
-                      <Text>{bindStatus.Pid}</Text>
+                      <Icon name="piNetwork" />
+                      <Text>{ellipsisMiddle(bindStatus.Pid, 12)}</Text>
                       <img src={SuccessaPng} alt="" />
                     </div>
                   </div>
                 ) : (
                   <Button onClick={() => setBindpiModal(true)}>
-                    {t("public.bind")}
+                    {t('public.bind')}
                   </Button>
                 )}
               </>
             )}
           </div>
           <HeaderTitle className="text-left w-100">
-            {t("public.tasks")}
+            {t('public.tasks')}
           </HeaderTitle>
           {bdLog.length
             ? bdLog.map((item: any) => (
@@ -157,11 +157,11 @@ export default function Airdrops() {
                   <div className="self-center flex gap-3">
                     <div className="grid self-center">
                       <Text>
-                        {item.key === "Pid Binding" && t("public.pidBinding")}
-                        {item.key === "Erc20 Wallet Binding" &&
-                          t("public.erc20WalletBinding")}
-                        {item.key === "Solana Wallet Binding" &&
-                          t("public.solanaWalletBinding")}
+                        {item.key === 'Pid Binding' && t('public.pidBinding')}
+                        {item.key === 'Erc20 Wallet Binding' &&
+                          t('public.erc20WalletBinding')}
+                        {item.key === 'Solana Wallet Binding' &&
+                          t('public.solanaWalletBinding')}
                       </Text>
                     </div>
                   </div>
@@ -170,10 +170,10 @@ export default function Airdrops() {
                   </Text>
                 </div>
               ))
-            : ""}
+            : ''}
         </div>
         <Loader />
       </Box>
     </>
-  );
+  )
 }
